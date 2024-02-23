3224b02f7399cc9dd5822a5a86662cad6c39150d
Pihu Gupta <pihugupa0501@gmail.com>
2024-02-22 19:43:04 -0800

    Display 3 photos by making the sample data an array and using map to loop over them

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,50 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const sampleDataForPhotoListItem = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  {
+    id: "2",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  },
+  {
+    id: "3",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  }
+];
+
+const renderPhotoListItems = () => {
+  return sampleDataForPhotoListItem.map(photo => (
+    <PhotoListItem key={photo.id}sampleDataForPhotoListItem={photo} />
+  ));
 };
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem sampleDataForPhotoListItem={sampleDataForPhotoListItem}/>
+      {renderPhotoListItems()}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,16 +10,19 @@ const PhotoListItem = (props) => {
     <>
     <div className="photo-list__item">
       <img className="photo-list__image" src={props.sampleDataForPhotoListItem.imageSource}/>
-    </div>
-    <div className="photo-list__user-details">
-      <img className="photo-list__user-profile" src={props.sampleDataForPhotoListItem.profile} />
-      <p className="photo-list__user-info">{props.sampleDataForPhotoListItem.username}</p>
-
-      <div className="photo-list__user-location">
-        <p>{props.sampleDataForPhotoListItem.location.city}, {props.sampleDataForPhotoListItem.location.country}</p> 
     
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.sampleDataForPhotoListItem.profile} />
+        <div>
+          <div className="photo-list__user-info">{props.sampleDataForPhotoListItem.username}</div>
+
+          <div className="photo-list__user-location">
+            {props.sampleDataForPhotoListItem.location.city}, {props.sampleDataForPhotoListItem.location.country}
+          </div>
+        </div>
       </div>
-    </div>
+
+    </div>  
     </>
   );
 };
