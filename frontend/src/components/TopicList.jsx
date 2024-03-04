d12aa302adccd65211e5664989c7adeaebd64329
Pihu Gupta <pihugupa0501@gmail.com>
2024-03-04 12:03:55 -0800

    Use mock data for photos and topics

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,27 +2,10 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
 
 const renderTopicListItems= () => {
-  return sampleDataForTopicList.map(topic => (
+  return topics.map(topic => (
     <TopicListItem key ={topic.id} title={topic.title} />
   ));
 }
