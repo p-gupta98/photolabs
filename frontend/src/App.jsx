99a6d65c2bb43378e14fc8cc975ae4947977b0d0
Pihu Gupta <pihugupa0501@gmail.com>
2024-03-06 15:42:47 -0800

    Add the PhotoDetailsModal to the App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.scss';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 import HomeRoute from 'routes/HomeRoute';
-
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 
 // Note: Rendering a single component to build components in isolation
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics} />
+      <PhotoDetailsModal photos={photos}/>
     </div>
   );
 };
