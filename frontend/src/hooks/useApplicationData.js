9b9de22fee4aebbe41b77cbe7d265a2dfcd36d03
Pihu Gupta <pihugupa0501@gmail.com>
2024-03-15 12:25:57 -0700

    Add initial useReducer code and create preliminary dispatch functions

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,35 +1,85 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useReducer } from "react";
+import { useEffect } from "react";
+
+
+/* insert app levels actions below */
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case FAV_PHOTO_ADDED:
+      return { /* insert logic */ }
+    { /* insert all relevant actions as case statements*/ }  
+    }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+
 
 function useApplicationData() {
 
-  const [state, setState] = useState({
+  const initialState = {
     displayModal: false,
     favorites: [],
     selectedPhoto: null,
-  });
+  }
   
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const favPhotoAdded = (id) => {
+    dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: { id }});
+  }
+
+  const favPhotoRemoved = (id) => {
+    dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: { id }});
+  }
+
+  const setPhotoData = (photoData) => {
+    dispatch( {type: ACTIONS.SET_PHOTO_DATA, payload: {photoData}} );
+  }
+
+  const setTopicData = (topicData) => {
+    dispatch( { type: ACTIONS.SET_TOPIC_DATA, payload: {topicData} } )
+  }
   
-  const setFavorites = (newFavorites) => {
-    setState((prev) => ({ ...prev, favorites: newFavorites }));
-  };
+  const selectPhoto = (photo) => {
+    dispatch({ type:ACTIONS.SELECT_PHOTO, payload: {photo} });
+  }
 
-  const setSelectedPhoto = (photo) => {
-    setState((prev) => ({ ...prev, selectedPhoto: photo }));
-  };
+  const displayPhotoDetails = () => {
+    dispatch( { type: ACTIONS.DISPLAY_PHOTO_DETAILS});
+  }
 
-  const setDisplayModal = () => {
-    setState((prev) => ({ ...prev, displayModal: true }));
-  };
+  // const setFavorites = (newFavorites) => {
+  //   setState((prev) => ({ ...prev, favorites: newFavorites }));
+  // };
 
-  const onClosePhotoDetailsModal = () => {
-    setState((prev) => ({ ...prev, displayModal: !prev.displayModal }));
-  };
+  // const setSelectedPhoto = (photo) => {
+  //   setState((prev) => ({ ...prev, selectedPhoto: photo }));
+  // };
 
-  const onPhotoSelect = (photo) => {
-    setSelectedPhoto(photo);
-    setDisplayModal(true);
-  };
+  // const setDisplayModal = () => {
+  //   setState((prev) => ({ ...prev, displayModal: true }));
+  // };
+
+  // const onClosePhotoDetailsModal = () => {
+  //   setState((prev) => ({ ...prev, displayModal: !prev.displayModal }));
+  // };
+
+  // const onPhotoSelect = (photo) => {
+  //   setSelectedPhoto(photo);
+  //   setDisplayModal(true);
+  // };
   
   
   
