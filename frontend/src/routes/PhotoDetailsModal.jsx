3dbd2a403cfb937722e7dce026ccfbaa907c866f
Pihu Gupta <pihugupta0501@gmail.com>
2024-03-23 15:58:04 -0700

    Fix the photoDetailsModal css

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -17,22 +17,30 @@ const PhotoDetailsModal = (props) => {
         <button className="photo-details-modal__close-button" onClick={props.onClosePhotoDetailsModal}>
           <img src={closeSymbol} alt="close symbol" />
         </button>
-        <PhotoFavButton onClick={handleClick} isLiked={props.favorites && props.favorites.includes(props.selectedPhoto.id)}/>
-        <img className="photo-details-modal__image" src={props.selectedPhoto.urls.full} alt={props.selectedPhoto.description} />
         
-        <div>
-            <div className="photo-list__user-details">
-              <img className="photo-list__user-profile" src={props.selectedPhoto.user.profile} />
-              <div className="photo-list__user-info">{props.selectedPhoto.user.username}</div>
-              <div className="photo-list__user-location">
-                {props.selectedPhoto.location.city}, {props.selectedPhoto.location.country}
-              </div>              
-            </div>
-        </div>
-        <div className="photo-details-modal__header">
-          Similar Photos
-        </div>
-        <PhotoList  photos={Object.values(props.selectedPhoto.similar_photos)} updateToFavPhotoIds={props.updateToFavPhotoIds} favorites={props.favorites}/>  
+          <PhotoFavButton onClick={handleClick} isLiked={props.favorites && props.favorites.includes(props.selectedPhoto.id)}/>
+        <div className='photo-details-modal__top-bar'>  
+          <img className="photo-details-modal__image" src={props.selectedPhoto.urls.full} alt={props.selectedPhoto.description} />
+        </div>   
+          <div>
+            <div className="photo-details-modal__photographer-details">
+              <img className="photo-details-modal__photographer-profile" src={props.selectedPhoto.user.profile} />
+              <div>
+                <div className="photo-details-modal__photographer-info">{props.selectedPhoto.user.username}</div>
+              
+                <div className="photo-details-modal__photographer-location">
+                  {props.selectedPhoto.location.city}, {props.selectedPhoto.location.country}
+                </div>
+              </div>  
+            </div>           
+          </div>
+         
+          <div className="photo-details-modal__header">
+            Similar Photos
+          </div>
+          <div className='photo-details-modal__images'>
+            <PhotoList  photos={Object.values(props.selectedPhoto.similar_photos)} updateToFavPhotoIds={props.updateToFavPhotoIds} favorites={props.favorites}/>
+          </div>
       </div>
       )
     )
